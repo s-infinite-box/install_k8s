3320b2735a89d38101c026595398f9375ad0352c
songwz <solidsea@outlook.com>
2024-09-25 16:38:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+https://cdn.dl.k8s.io/release/v1.30.2/kubernetes-server-linux-amd64.tar.gz
+
+1. 属性处理
+2. 节点处理
+3. cri安装
+4. k8s组件安装
+5. 集群初始化：kubeadm init / systemctl 启动组件
+6. cni 安装
+
+适配多节点 todo
+支持外部etcd todo
+apt安装使用kuberntes原始地址
+跳过kube-proxy
+
+[\[\033[1;3;35m\]\H\[\033[00m\]]🌻\[\033[1;32m\]\w \[\033[m\]`if git rev-parse >/dev/null 2>&1; then printf "[\[\033[1;36m\]%s\[\033[0m\]] " \`git branch --show-current --no-color\`; fi`\[\033[34m\]$\[\033[00m\]
---
+++ auto-install-kubernetes.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# 失败阻断
+#set -e
+# 打印脚本执行过程
+set -x
+# 预执行  可能会导致报错 /root/.bashrc: line 6: PS1: unbound variable
+# set -u
+
+### main
+# 当前目录
+export curr_dir=$(
+  cd "$(dirname "$0")" || exit 1
+  pwd
+)
+. $curr_dir/init_profile.sh
+
+. $curr_dir/init_node.sh
+
+. $curr_dir/install_cri.sh
+
+case $get_kube_type in
+  apt)
+    . install_kube_apt.sh
+    ;;
+  bin)
+    . install_kube_bin.sh
+    ;;
+  *)
+    echo '\033[31;1m WARNING: get_kube_type is empty, get kube by bin'
+    . install_kube_bin.sh
+    ;;
+esac
+
+case $init_type in
+  bin|BIN|systemd)
+    . init_cluster_by_systemd.sh
+  ;;
+  kubeadm)
+    . init_cluster_by_kubeadm.sh
+  ;;
+  # 手动安装，只初始化节点，默认按手动处理
+  Manual|manual|*)
+    echo 'no proces'
+    . init_cluster_by_kubeadm.sh
+  ;;
+esac
+
+set +x
